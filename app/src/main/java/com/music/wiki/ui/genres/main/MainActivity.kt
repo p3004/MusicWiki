8e3403f75849786ba54e2d57b22bedde3e38d5a3
Pallab Banerjee <50319302+p3004@users.noreply.github.com>
2021-02-19 00:08:38 +0530

    "Package hierarchy created"

--- app/src/main/java/com/music/wiki/ui/genres/main/MainActivity.kt
+++ app/src/main/java/com/music/wiki/ui/genres/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.music.wiki
+package com.music.wiki.ui.genres.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.music.wiki.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
