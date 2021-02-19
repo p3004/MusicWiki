5dc21b82d1ba1031c720e53cd3e51f8940ab506c
Pallab Banerjee <50319302+p3004@users.noreply.github.com>
2021-02-20 00:44:18 +0530

    "NetworkHelperImpl class used to check for the availability of internet connection on the device"

---
+++ app/src/main/java/com/music/wiki/utils/network/NetworkHelper.kt
@@ -0,0 +1,13 @@
+package com.music.wiki.utils.network
+
+/**
+ * Created by Pallab Banerjee on 2/20/2021.
+ */
+/**
+ * Interface to create FakeNetworkHelper for testing
+ * */
+
+interface NetworkHelper {
+
+    fun checkIsNetworkConnected(): Boolean
+}
