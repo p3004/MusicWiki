8e327a96bc0a09c8125aa5813be9e2a889be65db
Pallab Banerjee <50319302+p3004@users.noreply.github.com>
2021-02-21 00:07:32 +0530

    "TopGenre model created to hold the top genre data being fetched from API"

--- app/src/main/java/com/music/wiki/data/remote/ApiService.kt
+++ app/src/main/java/com/music/wiki/data/remote/ApiService.kt
@@ -1,12 +1,21 @@
 package com.music.wiki.data.remote
 
+import com.music.wiki.BuildConfig
+import com.music.wiki.data.model.TopTags
+import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 /**
  * Created by Pallab Banerjee on 2/20/2021.
  */
 interface ApiService {
 
-
+    @GET("")
+    fun getTopTags(
+        @Query("method") method : String = "tag.getTopTags",
+        @Query("api_key") api_key : String = BuildConfig.API_KEY,
+        @Query("format") format : String = "json"
+    ) : Single<TopTags>
 
 }
--- app/src/main/java/com/music/wiki/data/remote/Endpoint.kt
+++
@@ -1,9 +0,0 @@
-package com.music.wiki.data.remote
-
-/**
- * Created by Pallab Banerjee on 2/20/2021.
- */
-object Endpoint {
-
-    const val TOP_TAG = "method=tag.getTopTags&api_key=YOUR_API_KEY&format=json"
-}
