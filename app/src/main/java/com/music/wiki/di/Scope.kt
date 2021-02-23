6887f7f1bcbed96ad08068298401be28460f4de8
Pallab Banerjee <50319302+p3004@users.noreply.github.com>
2021-02-23 09:41:31 +0530

    "Scope file created to contain scopes required for dagger's dependency injection"

---
+++ app/src/main/java/com/music/wiki/di/Scope.kt
@@ -0,0 +1,16 @@
+package com.music.wiki.di
+
+import javax.inject.Scope
+
+/**
+ * Created by Pallab Banerjee on 2/23/2021.
+ */
+
+@Scope
+@Retention(AnnotationRetention.SOURCE)
+annotation class ActivityScope
+
+
+@Scope
+@Retention(AnnotationRetention.SOURCE)
+annotation class FragmentScope
