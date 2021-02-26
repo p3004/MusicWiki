ef0fadc63ec37866ce00d15791e9baa406c3dd7a
Pallab Banerjee <50319302+p3004@users.noreply.github.com>
2021-02-26 12:02:20 +0530

    "GenreTopArtistRepository created to get artist list from Last.fm api"

---
+++ app/src/main/java/com/music/wiki/data/repository/GetTopArtistRepository.kt
@@ -0,0 +1,15 @@
+package com.music.wiki.data.repository
+
+import com.music.wiki.data.model.info.artist.Artist
+import com.music.wiki.data.remote.ApiService
+import io.reactivex.rxjava3.core.Single
+import javax.inject.Inject
+
+/**
+ * Created by Pallab Banerjee on 2/26/2021.
+ */
+class GetTopArtistRepository @Inject constructor(private val apiService: ApiService) {
+
+    fun getTopArtist(tag : String) : Single<Artist> = apiService.getArtistList(tag = tag)
+
+}
