29bc713f111e50b4fbee3a75ef417c8ab4d9a91d
Pallab Banerjee <50319302+p3004@users.noreply.github.com>
2021-02-26 11:50:17 +0530

    "ApiService created for info page getting data from API"

--- app/src/main/java/com/music/wiki/data/remote/ApiService.kt
+++ app/src/main/java/com/music/wiki/data/remote/ApiService.kt
@@ -1,6 +1,10 @@
 package com.music.wiki.data.remote
 
 import com.music.wiki.BuildConfig
+import com.music.wiki.data.model.info.album.Album
+import com.music.wiki.data.model.info.artist.Artist
+import com.music.wiki.data.model.info.tag.Tags
+import com.music.wiki.data.model.info.track.Track
 import com.music.wiki.data.model.top.TopGenre
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
@@ -18,6 +22,39 @@ interface ApiService {
         @Query("format") format : String = "json"
     ) : Single<TopGenre>
 
+    @GET("2.0/")
+    fun getTagInfo(
+        @Query("method") method : String = "tag.getinfo",
+        @Query("tag") tag : String,
+        @Query("api_key") api_key : String = BuildConfig.API_KEY,
+        @Query("format") format : String = "json"
+    ) : Single<Tags>
 
+
+    @GET("2.0/")
+    fun getAlbumList(
+        @Query("method") method : String = "tag.gettopalbums",
+        @Query("tag") tag : String,
+        @Query("api_key") api_key : String = BuildConfig.API_KEY,
+        @Query("format") format : String = "json"
+    ) : Single<Album>
+
+
+    @GET("2.0/")
+    fun getArtistList(
+        @Query("method") method : String = "tag.gettopartists",
+        @Query("tag") tag : String,
+        @Query("api_key") api_key : String = BuildConfig.API_KEY,
+        @Query("format") format : String = "json"
+    ) : Single<Artist>
+
+
+    @GET("2.0/")
+    fun getTrackList(
+        @Query("method") method : String = "tag.gettoptracks",
+        @Query("tag") tag : String,
+        @Query("api_key") api_key : String = BuildConfig.API_KEY,
+        @Query("format") format : String = "json"
+    ) : Single<Track>
 
 }
